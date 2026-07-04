289082a544fbd0a70d978c090b3e3715e4ec676e
LionelTala <166503469+LionelTala@users.noreply.github.com>
2026-07-04 05:43:06 +0100

    Update env.ts

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,6 @@
 export const env = {
 apiUrl: 'https://cabbackend.alwaysdata.net/api',
-sanctumUrl: 'https://cabbackend.alwaysdata.net/api',
+sanctumUrl: 'https://cabbackend.alwaysdata.net/sanctum',
   pusher: {
     key: 'lionelskl@90',              // ⭐ Copier depuis .env (PUSHER_APP_KEY)
     cluster: 'mt1',                 // ⭐ Copier depuis .env (PUSHER_APP_CLUSTER)
