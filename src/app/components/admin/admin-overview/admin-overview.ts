e1042a6ace189e7ca03470e3f2279d4b46c40af5
LionelTala <lioneltala93230@gmail.com>
2026-07-04 10:43:51 +0100

    erreur arranger

--- src/app/components/admin/admin-overview/admin-overview.ts
+++ src/app/components/admin/admin-overview/admin-overview.ts
@@ -1,9 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-overview',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-overview.html',
   styleUrl: './admin-overview.css',
 })
-export class AdminOverview {}
+export class AdminOverview {
+   stats = {
+    totalEtudiants: 156,
+    totalFormations: 6,
+    totalVagues: 8,
+    candidaturesRecues: 24,
+    candidaturesEnAttente: 7
+  };
+
+  // Dernières activités (simples)
+  dernieresActivites = [
+    { action: 'Nouvelle candidature', details: 'Sikali Lionel - Infographie 2D', date: 'Il y a 2h' },
+    { action: 'Candidature validée', details: 'Tchoumi Paul - Réseaux & Maintenance', date: 'Il y a 5h' },
+    { action: 'Nouvelle vague', details: 'Vague Mars 2026 - Infographie', date: 'Il y a 1j' },
+    { action: 'Étudiant inscrit', details: 'Ndi Sophie - Vidéosurveillance', date: 'Il y a 2j' },
+    { action: 'Formation créée', details: 'Vidéosurveillance', date: 'Il y a 3j' }
+  ];
+}
